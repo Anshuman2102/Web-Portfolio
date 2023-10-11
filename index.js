454ca8aa3280baa7676b03f0833e16a26422c551
Anshuman Singh Adhikari <86787631+Anshuman2102@users.noreply.github.com>
2023-10-12 00:10:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const path = require('path')
+const nodemailer = require("nodemailer");
+const app=express()
+
+app.set("view engine", "ejs")
+app.set('views', __dirname);
+app.use(express.static(path.join(__dirname)));
+app.use(express.json())
+
+let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "portfoliomessage880@gmail.com",
+      pass: "ryeucajakvuaejec",
+    },
+  });
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.post('/mm',(req,res)=>{
+    if(!req.body.name || !req.body.email  || !req.body.message ){
+        return res.status(400).json({
+            success: -1,
+            message: " Some error occured",
+          });
+    }
+    const emailHTML = `
+         <html>
+         <body>
+         <h3>${req.body.name} Asked you something on Your Portfolio</h3>
+         <p>Email : '${req.body.email}'</p>
+         <p>Message : '${req.body.message}'</p>
+         </body>
+         </html>
+        `;
+        let mailDetails = {
+            from: "portfoliomessage880@gmail.com",
+            to: 'adhikarianshuman02@gmail.com',
+            subject: "New Message from Portfolio",
+            html: emailHTML,
+          };
+
+          mailTransporter.sendMail(mailDetails, function (err, data) {
+            if (err) {
+              console.log("Some error occured",err)
+              return res.status(400).json({
+                success: -1,
+                message: " Some error occured",
+              });
+            } else {
+              return res.status(200).json({
+    
+                message: "A mail sent successfully",
+              });
+            }
+          });
+})
+
+app.listen(3000,()=>{
+    console.log("Listening")
+});
